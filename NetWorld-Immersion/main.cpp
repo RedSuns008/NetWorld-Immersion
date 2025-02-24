6205bee1e91f7e1bf81333b60b578a7f7dd520b3
RedSuns0 <redsuns0@yandex.ru>
2025-02-24 14:33:24 +0300

    remove racket

--- NetWorld-Immersion/main.cpp
+++ NetWorld-Immersion/main.cpp
@@ -476,31 +476,15 @@ void ShowBattle() {
 
  }
 
-void ShowRacketAndBall() //TODO
+void ShowMapGame() //TODO
 {
     ShowBitmap(0, 0, window.width, window.height, hBack);//задний фон
 
-    ShowBitmap(racket.x - racket.width / 2., racket.y, racket.width, racket.height, racket.hBitmap);// ракетка игрока
-    
     for (int i = 0; i < enemycout; i++) {
         ShowBitmap(enemy1[i].x - enemy1[i].width / 2., enemy1[i].y, enemy1[i].width, enemy1[i].height, enemy1[i].hBitmap);
     }
     
     ShowBitmap(Mouse.x, Mouse.y, 1,1, raketka_bmp);
-}
-
-
-bool CheckCollision(racket_type r, enemycco e) //TODO
-{
-    auto dx = r.x - e.x;
-    auto dy = r.y - e.y;
-    auto dxy = sqrt(dx * dx + dy * dy);
-
-    if (dxy < (r.width + e.height) / 2.)
-    {
-        return true;
-    }
-    return false;
 }
 
 bool CheckCollisionMouse(enemycco e) //TODO
@@ -521,18 +505,20 @@ bool CheckCollisionMouse(enemycco e) //TODO
 void ProcessRoom() //TODO
 {
     for (int i = 0; i < enemycout; i++) {  // С ИФОВ ПЕРЕХОДИМ НА СВИТЧКЕЙС
-        if (CheckCollision(racket, enemy1[i]) == true) 
-        {
-            switch (enemy1[i].type) {
+        if (Mouse.L_butt) {
+            if (CheckCollisionMouse(enemy1[i]) == true)
+            {
+                switch (enemy1[i].type) {
                 case entity::lootchest:
                     game_mode = GameMode::loot;
-                     break;
+                    break;
                 case entity::enemy:
                     game_mode = GameMode::battle;
-                        break;
+                    break;
                 case entity::terminal:
                     game_mode = GameMode::terminal;
-                        break;
+                    break;
+                }
             }
         }
     }
@@ -635,7 +621,7 @@ void LootGame() {
 
 
 void MapGame() {
-    ShowRacketAndBall();//рисуем фон, ракетку и шарик
+    ShowMapGame();//рисуем фон, ракетку и шарик
     ShowScore();
     BitBlt(window.device_context, 0, 0, window.width, window.height, window.context, 0, 0, SRCCOPY);//копируем буфер в окно
     Sleep(16);//ждем 16 милисекунд (1/количество кадров в секунду) 
