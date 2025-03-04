b814f2ca1b00d6a47f6b16c9a94cd6f354ce7087
Данил Александров <fillindaniel2000@gmail.com>
2025-03-05 01:36:16 +0700

    ref,rename,restruct, dont work correctly, WIP

--- NetWorld-Immersion/main.cpp
+++ NetWorld-Immersion/main.cpp
@@ -177,8 +177,9 @@ void InitWindow() {
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nCmdShow) {
 
     InitWindow();
-    InitGame();
     InitMap();
+    InitGame();
+    
 
     while (!GetAsyncKeyState(VK_ESCAPE))  {
         currentTime = timeGetTime();
